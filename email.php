81051a01064ccffe4989df638ed1653d9056704f
darkblaro <darkblaro@gmail.com>
2020-10-02 19:30:28 -0500

    email.php added

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php
+    $doc=new DomDocument;
+    
+    $doc=new <Html:5>
+    </Html:5>
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+    $msg=$_POST['message'];
+    echo $name;
+?>
