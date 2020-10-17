bc9d47261021006ed309042482989a3e9fa47c13
darkblaro <darkblaro@gmail.com>
2020-10-17 13:42:00 -0500

    new funcs added to form.js

---
+++ form.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoader',function(){
+    let form=document.getElementById("emailform");
+    form.addEventListener("submit",formsend);
+    async function formsend(e){
+        e.preventDefault();
+
+        let formData=new FormData(form);
+        form.classList.add('_sending');
+        // let response=await fetch('sendmail.php',{
+        //     method:'POST',
+        //     body:formData
+        // });
+        // if(response.ok){
+        //     let result=await response.json();
+        //     alert(result.message);
+        //     formPreview.innerHTML='';
+        //     form.reset();
+        // }
+        // else{
+        //     alert("Somethig went wrong")
+        // }
+    }
+});
