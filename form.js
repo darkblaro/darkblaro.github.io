d126371676ffb2ebd9933eb595c1f215550bbed6
darkblaro <darkblaro@gmail.com>
2020-10-17 14:13:12 -0500

    phpmailer added

--- form.js
+++ form.js
@@ -6,18 +6,20 @@ document.addEventListener('DOMContentLoader',function(){
 
         let formData=new FormData(form);
         form.classList.add('_sending');
-        // let response=await fetch('sendmail.php',{
-        //     method:'POST',
-        //     body:formData
-        // });
-        // if(response.ok){
-        //     let result=await response.json();
-        //     alert(result.message);
-        //     formPreview.innerHTML='';
-        //     form.reset();
-        // }
-        // else{
-        //     alert("Somethig went wrong")
-        // }
+        let response=await fetch('sendmail.php',{
+            method:'POST',
+            body:formData
+        });
+        if(response.ok){
+            let result=await response.json();
+            alert(result.message);
+            formPreview.innerHTML='';
+            form.reset();
+            form.classList.remove('_sending');
+        }
+        else{
+            alert("Somethig went wrong");
+            form.classList.remove('_sending');
+        }
     }
 });
